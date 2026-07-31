1e6489907b2505beda12da049d61ee4bb5ad5d98
Dibyendu Barman <119417662+dibyendu-barman@users.noreply.github.com>
2026-07-31 07:56:14 +0530

    Update Pizza-Order-Practice.py

--- Pizza-Order-Practice.py
+++ Pizza-Order-Practice.py
@@ -1,1 +1,19 @@
+# This is a comment
 
+bill = 0
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ")
+if size == "S":
+    print("Small Pizza(S): $15")
+    bill = int(15)
+elif size == "M":
+    print("Medium Pizza(M): $20")
+elif size == "L":
+    print("Large Pizza(L): $25")
+else:
+    print("Please choose the correct size!")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+
+print(f"You needs to pay ${bill}")
